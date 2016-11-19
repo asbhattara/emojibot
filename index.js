a3c661099c12aaa8899d3f06aea291f05cbd1175
Ashmin <asbhattara>
2016-11-19 22:10:58 +0100

    Created Echo bot

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post('/webhook', function (req, res) {
         
         if (event.message && event.message.text) {           
         	var input = event.message.text;
-            var reply = data[input];
+            var reply = data[input][1];
     		sendMessage(event.sender.id, {text: reply});
         }
     }

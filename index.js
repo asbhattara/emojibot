cea2ce9902ca02be9de5b48b9d443f58b580d8a4
Ashmin <asbhattara>
2016-11-19 18:46:35 +0100

    Created Echo bot

--- index.js
+++ index.js
@@ -26,10 +26,10 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Ashmin: " + event.message.text});
+        var happy = '😀';
+        if (event.message && event.message.text) {           
 
-            var happy = ':)';
+            
     		sendMessage(event.sender.id, {text: "Ashmin: " + happy});
         }
     }
